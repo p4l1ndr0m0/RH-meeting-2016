a6129b57c2617723e23fd1e04f017a7d08aa1eea
p4l1ndr0m0 <billy@palindromo.com.mx>
2016-07-28 21:12:17 -0500

    Ventanas de Rally

---
+++ RH meeting 2016/rallyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  rallyViewController.swift
+//  RH meeting 2016
+//
+//  Created by Billy Jack Bates on 7/28/16.
+//  Copyright © 2016 Palindromo. All rights reserved.
+//
+
+import UIKit
+
+class rallyViewController: UIViewController {
+    
+    
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    @IBOutlet weak var c4: UIButton!
+    @IBOutlet weak var c5: UIButton!
+    @IBOutlet weak var c6: UIButton!
+    @IBOutlet weak var c7: UIButton!
+    @IBOutlet weak var c8: UIButton!
+    @IBOutlet weak var c9: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
